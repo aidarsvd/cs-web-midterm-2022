f4154640171edc33a11dc7021ecbefdc995d40ff
Aidar <achakiev2003@gmail.com>
2022-04-04 18:23:35 +0600

    project init

---
+++ src/main/java/pro/aidar/alatoo/AlatooApplication.java
@@ -0,0 +1,13 @@
+package pro.aidar.alatoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlatooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlatooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/aidar/alatoo/AlatooApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.aidar.alatoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlatooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
